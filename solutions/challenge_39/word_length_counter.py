e078ce6f6db7fdfdaf4797bd57ec2b280ee11aa5
Dorvil Lens Marc-Arthur <Dorvillensmarcarthur@gmail.com>
2025-01-10 02:23:34 -0500

    challenge_39

--- solutions/challenge_39/word_length_counter.py
+++ solutions/challenge_39/word_length_counter.py
@@ -4,13 +4,12 @@
 A module for calculating the lengths of words in a sentence.
 created: 01/07/2025
 @author: Arthur (Mr-Glucose)
-
 """
 
 import string
 
 
-def word_lengths(input_sentence):
+def word_lengths(input_sentence: str) -> list[int]:
     """
     Takes a sentence and returns a list of word lengths, ignoring punctuation.
 
@@ -19,6 +18,18 @@ def word_lengths(input_sentence):
 
     Returns:
         list: A list of integers representing word lengths.
+
+    Examples:
+        >>> word_lengths("Hello world!")
+        [5, 5]
+        >>> word_lengths("Python is awesome.")
+        [6, 2, 7]
+        >>> word_lengths("co-op re-enter.")
+        [4, 7]
+        >>> word_lengths("")
+        []
+        >>> word_lengths("...?!")
+        []
     """
     words = input_sentence.translate(str.maketrans("", "", string.punctuation)).split()
     return [len(word) for word in words]
